64363a5cb4edad95bca89b89e5a2403774e514d2
Pablo Veronezi <pabloorempuller@hotmail.com>
2022-03-22 10:40:48 -0300

    Projeto finalizado

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# urna-eletronica
-Projeto urna eletrônica digital
+# Urna Eletrônica Digital
+
+## Projeto desenvolvido aplicando fundamentos básicos de Java Script, CSS e HTML, seguindo a função tradicional de uma urna eletrônica.
+
+<img src="./src/img/urna-eletronica.gif" style="border-radius: .5rem">
+
---
+++ src/js/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+  {
+    titulo: "DEPUTADO ESTADUAL",
+    numeros: 5,
+    candidatos: [
+      {
+        numero: "12345",
+        nome: "Pablo Veronezi",
+        partido: "OCV",
+        fotos: [
+          { url: "avatar_1.svg", legenda: "Deputado-Estadual" }
+        ]
+      },
+      {
+        numero: "54321",
+        nome: "Sarah Belo",
+        partido: "BRP",
+        fotos: [
+          { url: "avatar_2.svg", legenda: "Deputada-Estadual" }
+        ]
+      }
+    ]
+  },
+  {
+    titulo: "PRESIDENTE",
+    numeros: 2,
+    candidatos: [
+      {
+        numero: "11",
+        nome: "Pablo Veronezi",
+        partido: "OCV",
+        vice: "Sarah Belo",
+        fotos: [
+          { url: "avatar_1.svg", legenda: "Presidente" },
+          { url: "avatar_2.svg", legenda: "Vice-Presidente", small: true }
+        ]
+      },
+      {
+        numero: "26",
+        nome: "Gabriel Sampaio",
+        partido: "SLA",
+        vice: "Beatriz Ladeira",
+        fotos: [
+          { url: "avatar_3.svg", legenda: "Presidente" },
+          { url: "avatar_6.svg", legenda: "Vice-Presidente", small: true }
+        ]
+      }
+    ]
+  }
+];
